531bb6fad09d150dd3749e9e4ebd017cb0e1f18a
wiredacorn <dev@wiredacorn.com>
2020-07-12 23:40:22 -0600

    change blog directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
         resolve: `gatsby-source-filesystem`,
         options: {
           name: `markdown-pages`,
-          path: `${__dirname}/blog`,
+          path: `${__dirname}/blog/markdown-pages`,
         },
     },
     `gatsby-transformer-remark`,
